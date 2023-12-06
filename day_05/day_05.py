7bde94bd3dec5d4a1dd442905190cc16108e37eb
NickSwainston <nickaswainston@gmail.com>
2023-12-06 16:07:59 +0800

    Did day 5 with some reverse map brute force

--- day_05/day_05.py
+++ day_05/day_05.py
@@ -1,5 +1,5 @@
-with open('example.txt', 'r') as file:
-# with open('input.txt', 'r') as file:
+# with open('example.txt', 'r') as file:
+with open('input.txt', 'r') as file:
     # Read the lines
     lines = file.readlines()
     lines = [line.strip() for line in lines]
@@ -43,68 +43,40 @@ for seed in seeds:
 
 print(f"Part 1 {min(locations)}")
 
-locations = []
-for i in range(len(seeds)//2):
-    edges = [(seeds[i], seeds[i] + seeds[i+1] - 1)]
-    print(f"Seed range: {edges}")
-    for current_map in all_maps:
-        this_maps_edges = []
-        for input_start, input_end in edges:
-            print(f"    Input range: {input_start} - {input_end}")
-            if (input_start, input_end) not in this_maps_edges:
-                this_maps_edges.append((input_start, input_end))
-            for start, end, change in current_map:
-                print(f"        Map range: {start} - {end}")
-                if start < input_start and input_end < end:
-                    print("        Inside")
-                    pass
-                elif input_start < start and end < input_end:
-                    print("        Outside")
-                    try:
-                        this_maps_edges.remove((input_start, input_end))
-                    except ValueError:
-                        pass
-                    this_maps_edges.append((input_start, start -1))
-                    this_maps_edges.append((start, end -1))
-                    this_maps_edges.append((end, input_end -1))
 
-                elif input_start < start < input_end:
-                    print("        Start inside")
-                    try:
-                        this_maps_edges.remove((input_start, input_end))
-                    except ValueError:
-                        pass
-                    this_maps_edges.append((input_start, start -1))
-                    this_maps_edges.append((start, input_end))
-                elif input_start < end < input_end:
-                    print("        End inside")
-                    try:
-                        this_maps_edges.remove((input_start, input_end))
-                    except ValueError:
-                        pass
-                    this_maps_edges.append((input_start, end -1))
-                    this_maps_edges.append((end, input_end))
-                else:
-                    print("        No overlap")
-                    pass
-                print(f"        this_maps_edges: {this_maps_edges}")
+inverse_maps = []
+for chunk in reversed(chunks[1:]):
+    print(f"Chunk: {chunk}")
+    current_map = []
+    for line in chunk[1:]:
+        dest_start, source_start, length = [int(value) for value in line.split()]
+        current_map.append((dest_start, dest_start + length, source_start- dest_start))
+    inverse_maps.append(current_map)
+print(all_maps)
+print(inverse_maps)
 
-        new_edge_values = []
-        for edge_start, edge_stop in this_maps_edges:
-            for start, end, change in current_map:
-                if start <= edge_start <= end:
-                    edge_start += change
-                    break
-            for start, end, change in current_map:
-                if start <= edge_stop <= end:
-                    edge_stop += change
-                    break
-            new_edge_values.append((edge_start, edge_stop))
-        edges = new_edge_values
-        print(f"New edges: {edges}")
-    print(f"Final edges: {edges}")
-    for start, end in edges:
-        locations.append(start)
-        locations.append(end)
 
-print(f"Part 2 {min(locations)}")
+possible_location = 0
+found_seed = False
+while not found_seed:
+    possible_location += 1
+    print(f"\nPossible location: {possible_location}")
+    current_value = possible_location
+    for inverse_map in inverse_maps:
+        for start, end, change in inverse_map:
+            if start <= current_value <= end:
+                current_value += change
+                break
+    # print(f"Seed value: {current_value}")
+    is_a_seed = False
+    for i in range(len(seeds)//2):
+        left_range, right_range = (seeds[2*i], seeds[2*i] + seeds[2*i+1] - 1)
+        # print(f"Left range: {left_range}, right range: {right_range}")
+        if left_range <= current_value <= right_range:
+            # print(f"Found seed: {seeds[i]}")
+            is_a_seed = True
+            break
+    if is_a_seed:
+        found_seed = True
+
+print(f"Part 2 {possible_location}")
